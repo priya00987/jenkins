bd8275afb989fd02226c76c4665b033651ae2bb2
priya00987 <mittalpriya459@gmail.com>
2025-10-05 02:29:25 +0530

    code push

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+package jenkins;
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Welcome to Java programming!");
         System.out.println("Let's write some code.");
         System.out.println("Goodbye!");
+        System.out.println("checking code push to github");
     }
 }
