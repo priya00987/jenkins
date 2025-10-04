51caa7d138c3eb487f9c6b5f108e3452eb45fd00
priya00987 <mittalpriya459@gmail.com>
2025-10-05 02:07:41 +0530

    inital commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to Java programming!");
+        System.out.println("Let's write some code.");
+        System.out.println("Goodbye!");
+    }
+}
